b744ecfa86a1aefb8e368a8c57b200165b8f3f1d
J. Graves <jwgravesfl@gmail.com>
2018-09-29 09:57:39 -0600

    17

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom' 
+import { Route, HashRouter, Switch } from 'react-router-dom' 
 
 import NavBar from './NavBar'
 
