8d496451af5eec0a59678d958122d1f8a3ce5265
J. Graves <jwgravesfl@gmail.com>
2018-09-28 20:13:02 -0600

    4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 import styled from 'styled-components'
 
@@ -13,3 +14,4 @@ const MainStyleDiv = styled.div `
 `
 
 ReactDOM.render(<MainStyleDiv><App /></MainStyleDiv>, document.getElementById('root'));
+registerServiceWorker();
